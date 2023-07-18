a9931ae7201a538208ffbe0703381ba5224ef2c9
Sant16 <87797542+Sant16@users.noreply.github.com>
2023-07-17 19:44:36 -0500

    Create hello_world_st.py

---
+++ hello_world_st.py
@@ -0,0 +1,8 @@
+# COdigo basico para escribir mensajes en una app web
+
+import streamlit as st
+import time
+st.title("Hello world")
+st.subheader('and Mars')
+st.text('Im going to tech you how to use this')
+st.markdown("___")
